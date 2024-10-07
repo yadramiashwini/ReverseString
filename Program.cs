9abb548da261a3a39157f8245751628b8b9d3a90
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-07 14:21:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ReverseString
+{
+    public class StringReverse
+    {
+        public static string ReverseString(string s)
+        {
+            char[] ReversedArr = s.ToCharArray();
+
+            Array.Reverse(ReversedArr);
+
+            return new string(ReversedArr);
+
+        }
+        public static bool isPalindrome(string s)
+        {
+            if (s == ReverseString(s))
+            {
+                return true;
+            }
+            return false;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter string:");
+            string input = Console.ReadLine();
+        
+            string str = ReverseString(input);
+            Console.WriteLine("reversed string is:"+str);
+
+            if (isPalindrome(input) == false)
+            {
+                Console.WriteLine("not a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("palindrome");
+            }
+        }
+    }
+}
